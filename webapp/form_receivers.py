586bd10cdb2a425342872aa58c569840605b71a7
www-data <www-data@ip-10-154-156-62.ec2.internal>
2013-09-05 12:51:04 -0500

    Test run generated code

--- webapp/form_receivers.py
+++ webapp/form_receivers.py
@@ -37,7 +37,7 @@ def sign_up(request):
                             password=request.POST['password'])
         auth_login(request, user)
 
-        redirect_url = reverse('webapp.pages.user_profile', args=(None.id, ))
+        redirect_url = reverse('webapp.pages.user_profile', args=(user.id, ))
         return JsonResponse(data={'redirect_to': redirect_url})
 
     return JsonResponse(errors=form.errors)
